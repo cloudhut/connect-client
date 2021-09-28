81e47158359ec0cc808620d7475609f097d122c3
Holger Adam <holger.adam@rewe-digital.com>
2021-09-28 11:42:52 +0200

    fix return type of put validate connector config

--- put_validate_connector_config.go
+++ put_validate_connector_config.go
@@ -8,20 +8,20 @@ type ValidateConnectorConfigOptions struct {
 	Config map[string]string
 }
 
-type ValidationResultConfig struct {
+type ConnectorValidationResultConfig struct {
 	Definition map[string]string `json:"definition"`
 	Value      map[string]string `json:"value"`
 }
 
 type ConnectorValidationResult struct {
-	Name       string                   `json:"name"`
-	ErrorCount int                      `json:"error_count"`
-	Groups     []string                 `json:"groups"`
-	Configs    []ValidationResultConfig `json:"configs"`
+	Name       string                            `json:"name"`
+	ErrorCount int                               `json:"error_count"`
+	Groups     []string                          `json:"groups"`
+	Configs    []ConnectorValidationResultConfig `json:"configs"`
 }
 
-func (c *Client) ValidateConnectorConfig(ctx context.Context, pluginClassName string, options ValidateConnectorConfigOptions) (ValidationResultConfig, error) {
-	var validationResultConfig ValidationResultConfig
+func (c *Client) ValidateConnectorConfig(ctx context.Context, pluginClassName string, options ValidateConnectorConfigOptions) (ConnectorValidationResult, error) {
+	var validationResultConfig ConnectorValidationResult
 	response, err := c.client.NewRequest().
 		SetContext(ctx).
 		SetResult(&validationResultConfig).
@@ -30,12 +30,12 @@ func (c *Client) ValidateConnectorConfig(ctx context.Context, pluginClassName st
 		SetBody(options.Config).
 		Put("/connector-plugins/{pluginClassName}/config/validate")
 	if err != nil {
-		return ValidationResultConfig{}, err
+		return ConnectorValidationResult{}, err
 	}
 
 	err = getErrorFromResponse(response)
 	if err != nil {
-		return ValidationResultConfig{}, err
+		return ConnectorValidationResult{}, err
 	}
 
 	return validationResultConfig, nil
