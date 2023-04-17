963e5bcdfee7f7d6348cb3ef8def16eb81f1775b
Bojan <dbojan@gmail.com>
2023-04-17 09:42:47 -0300

    feature: add restart connector query options (#1)
    * feature: add restart connector query options

--- restart_connector.go
+++ restart_connector.go
@@ -2,13 +2,29 @@ package connect
 
 import (
 	"context"
+	"strconv"
 )
 
-func (c *Client) RestartConnector(ctx context.Context, connectorName string) error {
+// RestartConnectorOptions describe the available options to restart connectors.
+type RestartConnectorOptions struct {
+	// Specifies whether to restart the connector instance and task instances
+	// or just the connector instance. Default is false.
+	IncludeTasks bool
+	// Specifies whether to restart just the instances with a FAILED status
+	// or all instances. Default is false.
+	OnlyFailed bool
+}
+
+// // RestartConnector restart the connector.
+func (c *Client) RestartConnector(ctx context.Context, connectorName string, options RestartConnectorOptions) error {
 	response, err := c.client.NewRequest().
 		SetContext(ctx).
 		SetError(ApiError{}).
 		SetPathParam("connector", connectorName).
+		SetQueryParams(map[string]string{
+			"includeTasks": strconv.FormatBool(options.IncludeTasks),
+			"onlyFailed":   strconv.FormatBool(options.OnlyFailed),
+		}).
 		Post("/connectors/{connector}/restart")
 	if err != nil {
 		return err
